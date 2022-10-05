f72a1b9c4d5c9e5ac9a1e0e43c6502b6c2e92c20
PARK JEONG HEE <75747197+lyeong-gwa@users.noreply.github.com>
2022-10-05 15:21:41 +0900

    [Bronze V] Title: Hello World, Time: 68 ms, Memory: 11288 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		System.out.println("Hello World!");
+	}
+	
+
+	
+}
